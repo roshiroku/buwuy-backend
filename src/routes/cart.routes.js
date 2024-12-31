7ee8f9ffba06fb647029a36e7e65143be983c2c7
roshiroku <shir.oraan@gmail.com>
2024-12-31 23:00:17 +0200

    update cart routes

--- src/routes/cart.routes.js
+++ src/routes/cart.routes.js
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import multer from 'multer';
 import { protect } from '../middleware/auth.middleware.js';
 import { updateCart, getCart, clearCart } from '../controllers/cart.controller.js';
 
@@ -9,10 +10,15 @@ const router = Router();
 // @access  User
 router.get('/', protect, getCart);
 
+// @route   POST /api/cart
+// @desc    Create cart
+// @access  User
+router.post('/', protect, multer().none(), updateCart);
+
 // @route   PUT /api/cart
 // @desc    Update cart
 // @access  User
-router.put('/', protect, updateCart);
+router.put('/', protect, multer().none(), updateCart);
 
 // @route   DELETE /api/cart
 // @desc    Clear cart
