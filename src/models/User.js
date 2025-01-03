bc8396d59de0856ebdaa1c4a3a62a9e3c27ec668
roshiroku <shir.oraan@gmail.com>
2025-01-04 00:20:39 +0200

    update user model name

--- src/models/User.js
+++ src/models/User.js
@@ -2,25 +2,9 @@ import { Schema, model } from 'mongoose';
 import { fileProp, hashProp } from '../utils/schema.utils.js';
 
 const userSchema = new Schema({
-  name: {
-    first: {
-      type: String,
-      required: true
-    },
-    last: {
-      type: String,
-      required: true
-    }
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
   role: {
     type: String,
     enum: ['admin', 'moderator', 'user'],
