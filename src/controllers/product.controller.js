4f7d996b39af93b3dd42e898fa4272a07432d7f5
roshiroku <shir.oraan@gmail.com>
2025-01-05 00:44:47 +0200

    update product controller

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -3,23 +3,19 @@ import Product from '../models/Product.js';
 import { deleteFile, normalizeFilePath } from '../utils/file.utils.js';
 
 function handleUploads(req) {
-  const { images = [], variants = [] } = req.body;
+  const { images = [] } = req.body;
   const { files = [] } = req;
 
-  [images, ...variants.map(({ images }) => images)].forEach((images, i) => {
-    images.forEach((image, j) => {
-      if (!image.src) {
-        const index = files.findIndex(({ fieldname }) => {
-          return fieldname.match(new RegExp(
-            `${i ? `variants\\[${i - 1}\\]\\[images\\]` : 'images'}\\[${j}\\]\\[src\\]`
-          ));
-        });
-        if (index > -1) {
-          image.src = normalizeFilePath(files[index]);
-          files.splice(index, 1);
-        }
+  images.forEach((image, i) => {
+    if (!image.src) {
+      const index = files.findIndex(({ fieldname }) => {
+        return fieldname.match(new RegExp(`images\\[${i}\\]\\[src\\]`));
+      });
+      if (index > -1) {
+        image.src = normalizeFilePath(files[index]);
+        files.splice(index, 1);
       }
-    });
+    }
   });
 
   while (files.length) {
@@ -31,10 +27,11 @@ function handleUploads(req) {
 export async function createProduct(req, res) {
   try {
     handleUploads(req);
-    const { name, description, category, price, stock, images = [], tags = [] } = req.body;
+    const { name, byline, description, category, price, stock, images = [], tags = [] } = req.body;
 
     const product = await Product.create({
       name,
+      byline,
       description,
       price,
       stock,
@@ -119,7 +116,7 @@ export async function getProduct(req, res) {
 export async function updateProduct(req, res) {
   try {
     handleUploads(req);
-    const { name, description, category, price, stock, images = [], tags = [], variants = [] } = req.body;
+    const { name, byline, description, category, price, stock, images = [], tags = [] } = req.body;
     const product = await Product.findById(req.params.id);
 
     if (!product) {
@@ -128,13 +125,13 @@ export async function updateProduct(req, res) {
     }
 
     product.name = name ?? product.name;
+    product.byline = byline ?? product.byline;
     product.description = description ?? product.description;
     product.price = price ?? product.price;
     product.stock = stock ?? product.stock;
     product.images = images ?? product.images;
     product.category = category ?? product.category;
     product.tags = tags ?? product.tags;
-    product.variants = variants ?? product.variants;
 
     await product.save();
     res.json(product);
