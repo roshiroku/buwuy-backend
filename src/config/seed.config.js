f6ec45b3c7015711182abffcd6fac6adefcf5eec
roshiroku <shir.oraan@gmail.com>
2025-01-05 15:55:13 +0200

    update seeding

--- src/config/seed.config.js
+++ src/config/seed.config.js
@@ -5,6 +5,7 @@ import User from '../models/User.js';
 import Tag from '../models/Tag.js';
 import Category from '../models/Category.js';
 import Product from '../models/Product.js';
+import Order from '../models/Order.js';
 import { copyFilesSync } from '../utils/file.utils.js';
 
 const seed = config.has('seed') && config.get('seed') || {};
@@ -12,6 +13,7 @@ const userData = seed.users && JSON.parse(fs.readFileSync(path.resolve(seed.user
 const tagData = seed.tags && JSON.parse(fs.readFileSync(path.resolve(seed.tags)));
 const categoryData = seed.categories && JSON.parse(fs.readFileSync(path.resolve(seed.categories)));
 const productData = seed.products && JSON.parse(fs.readFileSync(path.resolve(seed.products)));
+const orderData = seed.orders && JSON.parse(fs.readFileSync(path.resolve(seed.orders)));
 const { assets } = seed;
 
 // Async function to seed data
@@ -68,6 +70,38 @@ export default async function seedData() {
           });
         }
       });
+    }
+
+    // Seed Orders
+    if (orderData && !await Order.countDocuments()) {
+      isSeeding = true;
+      const users = await User.find({}, 'email _id');
+      const products = await Product.find({}, 'slug _id name price');
+
+      const orders = orderData.map((order) => {
+        const user = users.find(({ email }) => email === order.user);
+        const items = order.items.map(item => {
+          const product = products.find(p => p.slug === item.product);
+          return {
+            product: product._id,
+            name: product.name,
+            price: product.price,
+            amount: item.amount
+          };
+        });
+
+        return {
+          user: user?._id || null,
+          contact: order.contact,
+          address: order.address,
+          items,
+          subtotal: items.reduce((total, item) => total + item.price * item.amount, 0),
+          status: order.status
+        };
+      });
+
+      await Order.insertMany(orders);
+      console.log('Orders seeded');
     }
 
     if (isSeeding) {
